935c1aeee035134d3dda36680cee3eafbab94844
723xulele <xll723321@gmail.com>
2020-10-22 00:54:12 +0800

    i am demo4

---
+++ src/main/java/com/qfedu/controller/Demo4.java
@@ -0,0 +1,14 @@
+package com.qfedu.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author : XuLeLe
+ * @Date : 2020/10/22/0:53
+ * @Description:
+ */
+public class Demo4 {
+    public static void main(String[] args) {
+        System.out.println("demo4");
+    }
+}

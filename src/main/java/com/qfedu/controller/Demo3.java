cfdcc71bd0fd659d7d0c4a95033a58db6f331ba8
723xulele <xll723321@gmail.com>
2020-10-22 00:50:23 +0800

    i am xulele

---
+++ src/main/java/com/qfedu/controller/Demo3.java
@@ -0,0 +1,14 @@
+package com.qfedu.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author : XuLeLe
+ * @Date : 2020/10/22/0:49
+ * @Description:
+ */
+public class Demo3 {
+    public static void main(String[] args) {
+        System.out.println("i am xulele");
+    }
+}

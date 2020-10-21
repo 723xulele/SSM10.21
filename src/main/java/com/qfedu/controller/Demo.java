13e884382f4c17256d6e997a97bd908b9b709673
723xulele <xll723321@gmail.com>
2020-10-22 00:48:53 +0800

    this is Demo hello world

---
+++ src/main/java/com/qfedu/controller/Demo.java
@@ -0,0 +1,14 @@
+package com.qfedu.controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author : XuLeLe
+ * @Date : 2020/10/22/0:46
+ * @Description:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
